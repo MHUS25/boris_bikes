6db776be1b3759664262904ff4097013d4542c23
Felix Dalby Bowler <felixdb@Felixs-MacBook-Pro.local>
2018-10-03 15:30:06 +0100

    attr_reader bike added, dock functionality stores bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,17 @@ require 'docking_station.rb'
 
 describe DockingStation do
     it { is_expected.to respond_to :release_bike}
+
     it 'releases working bikes' do
       bike = subject.release_bike
       expect(bike).to be_working
+    end
+
+    it { is_expected.to respond_to(:dock).with(1).argument}
+
+    it 'Bike in dock is equal to the bike that was just docked' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq(bike)
     end
 end
